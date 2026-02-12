987db921020119bb3a1528d40c372c49658ff0d5
Pranav Kumar <pranavsingh9471@gmail.com>
2026-02-12 23:14:16 +0530

    login page modified

--- frontend/tripvault-frontend/app/login/page.tsx
+++ frontend/tripvault-frontend/app/login/page.tsx
@@ -30,3 +30,36 @@ export default function LoginPage() {
     </div>
   );
 }
+
+// "use client";
+
+// import { useEffect, useState } from "react";
+// import { apiFetch } from "@/lib/api";
+// import { useAuth } from "@/lib/auth";
+// import { useRouter } from "next/navigation";
+
+// export default function LoginPage() {
+//   const [users, setUsers] = useState<any[]>([]);
+//   const { login } = useAuth();
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     apiFetch("/users").then(setUsers);
+//   }, []);
+
+//   function selectUser(id: string) {
+//     login(id);
+//     router.push("/trips");
+//   }
+
+//   return (
+//     <div>
+//       <h2>Select User</h2>
+//       {users.map((u) => (
+//         <button key={u.id} onClick={() => selectUser(u.id)}>
+//           {u.email}
+//         </button>
+//       ))}
+//     </div>
+//   );
+// }
